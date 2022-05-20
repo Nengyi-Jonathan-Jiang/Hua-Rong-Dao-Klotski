19d8be075681cd4f085e3ed9d482461464f1f0f4
Jonathan Jiang <89473479+Nengyi-Jonathan-Jiang@users.noreply.github.com>
2022-05-20 07:32:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -194,8 +194,6 @@ game.addScene("Level", new Scene(function(gotoScene, data){
 				let dx = e.clientX - el_game_game_view.getBoundingClientRect().x - data.mouseDownX;
 				let dy = e.clientY - el_game_game_view.getBoundingClientRect().y - data.mouseDownY;
 			
-				if(dx * dx + dy * dy <= 2) return;	//Prevent small swipe
-				
 				let angle = (Math.round(Math.atan2(dy, dx) / Math.PI  * 2) + 2) & 3;
 				let piece = data.selectedPiece;
 			
@@ -214,7 +212,7 @@ game.addScene("Level", new Scene(function(gotoScene, data){
 				let dx = e.clientX - el_game_game_view.getBoundingClientRect().x - data.mouseDownX;
 				let dy = e.clientY - el_game_game_view.getBoundingClientRect().y - data.mouseDownY;
 				
-				if(dx * dx + dy * dy <= 2) return;	//Prevent small swipe
+				if(dx * dx + dy * dy <= 1) return;	//Prevent small swipe
 
 				let angle = (Math.round(Math.atan2(dy, dx) / Math.PI  * 2) + 2) & 3;
 				let piece = data.selectedPiece;
